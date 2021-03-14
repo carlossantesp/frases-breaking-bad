48a8cd134b5f7f5df352dbb8d9bd1b3b6ed6c8dc
Carlos Santillan Espinoza <carlos.santesp@gmail.com>
2021-03-14 16:52:57 -0400

    Redesign UI del proyecto

--- src/App.js
+++ src/App.js
@@ -1,34 +1,94 @@
 import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 import Frase from "./components/Frase";
+import breakingBad from "./breaking-bad.jpg";
+import logo from "./logo.svg";
 
-const Contenedor = styled.div`
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-areas: "logo" "img" "frase";
+  padding: 1rem;
+  max-width: 400px;
+  align-items: stretch;
+  @media (min-width: 768px) {
+    grid-template-areas: "logo logo" "img frase";
+    grid-template-columns: 1fr 1fr;
+    max-width: 900px;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 2fr;
+  }
+  @media (min-width: 1440px) {
+    max-width: 1000px;
+  }
+`;
+
+const LogoWrapper = styled.div`
+  grid-area: logo;
+  margin-bottom: 1rem;
+  @media (min-width: 768px) {
+    margin-bottom: 3rem;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  grid-area: img;
+  width: 100%;
+  max-height: 20rem;
+  overflow: hidden;
+  @media (min-width: 768px) {
+    box-shadow: 0 6px 4px rgba(0, 0, 0, 0.2);
+  }
+  @media (min-width: 992px) {
+    max-height: initial;
+    height: auto;
+  }
+`;
+
+const FraseWrapper = styled.section`
+  grid-area: frase;
   display: flex;
-  align-items: center;
-  padding-top: 5rem;
   flex-direction: column;
+  justify-content: space-between;
+  padding: 2rem 1rem;
+  background-color: rgba(255, 255, 255, 0.2);
+  box-shadow: 0 6px 4px rgba(0, 0, 0, 0.2);
+  @media (min-width: 992px) {
+    padding: 2rem;
+  }
+`;
+
+const Logo = styled.img`
+  width: 10rem;
+  margin-inline: auto;
+  @media (min-width: 768px) {
+    width: 12em;
+  }
+`;
+
+const ImagenApp = styled.img`
+  height: 100%;
+  object-fit: cover;
 `;
 
 const Boton = styled.button`
-  background: -webkit-linear-gradient(
-    top left,
-    #007d35 0%,
-    #007d35 40%,
-    #0f574e 100%
-  );
-  background-size: 325px;
-  font-family: Arial, Helvetica, sans-serif;
+  cursor: pointer;
+  border: none;
+  display: block;
+  padding: 1rem 2rem;
+  background-color: rgba(0, 0, 0, 0.9);
   color: #fff;
-  margin-top: 3rem;
-  padding: 1rem 3rem;
-  font-size: 2rem;
-  border: 2px solid black;
-  transition: background-size 0.8s ease;
-  :hover {
-    cursor: pointer;
-    background-size: 400px;
+  margin-top: 1rem;
+  border-radius: 2rem;
+  margin-inline: auto;
+  text-transform: uppercase;
+  font-weight: bold;
+  transition: background 0.3s ease;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.65);
   }
 `;
+
 function App() {
   const [frase, setFrase] = useState({});
 
@@ -47,10 +107,18 @@ function App() {
   }, []);
 
   return (
-    <Contenedor>
-      <Frase frase={frase} />
-      <Boton onClick={consultarAPI}>Obtener Frase</Boton>
-    </Contenedor>
+    <Wrapper>
+      <LogoWrapper>
+        <Logo src={logo} alt="Logo breaking bad" />
+      </LogoWrapper>
+      <ImageWrapper>
+        <ImagenApp src={breakingBad} alt="walter breaking bad" />
+      </ImageWrapper>
+      <FraseWrapper>
+        <Frase frase={frase} />
+        <Boton onClick={consultarAPI}>Obtener Frase</Boton>
+      </FraseWrapper>
+    </Wrapper>
   );
 }
 
--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,37 +1,37 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-const ContenedorFrase = styled.div`
-  padding: 3rem;
-  border-radius: 0.5rem;
-  background-color: #fff;
-  max-width: 800px;
-  @media (min-width: 992px) {
-    margin-top: 10rem;
-  }
-
+const ContenedorFrase = styled.article`
+  margin-bottom: 2rem;
   h1 {
-    font-family: Arial, Helvetica, sans-serif;
-    text-align: center;
+    font-size: 1.5rem;
+    margin-bottom: 0.5rem;
+    font-weight: bold;
     position: relative;
-    padding-left: 4rem;
     &::before {
       content: open-quote;
-      font-size: 10rem;
+      font-size: 3rem;
       color: black;
-      position: absolute;
-      left: -1rem;
-      top: -2rem;
+      position: relative;
+      left: 0;
+      top: 1rem;
     }
   }
-
   p {
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
-    font-size: 1.6rem;
-    font-weight: bold;
     text-align: right;
-    color: #666;
-    margin-top: 2rem;
+    color: #444;
+  }
+  @media (min-width: 1440px) {
+    h1 {
+      font-size: 2rem;
+      &::before {
+        content: open-quote;
+        font-size: 4rem;
+      }
+    }
+    p {
+      font-size: 1.25rem;
+    }
   }
 `;
 
